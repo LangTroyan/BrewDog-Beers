5805bd29e37d8eddbd0562b84ff4adb4674c5764
Dylan Trujillo <langtroyan@gmail.com>
2019-08-20 18:57:23 +0200

    Initial Commit

---
+++ BrewDog Beers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrewDog Beers
+//
+//  Created by Lang Troyan on 20/08/2019.
+//  Copyright © 2019 Lang Troyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
